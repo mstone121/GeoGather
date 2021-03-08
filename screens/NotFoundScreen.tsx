24a203b028307ec03750c2ce0d77eb46006a0657
Matt Stone <mstone@mrn.org>
2021-03-07 22:09:22 -0600

    Use Material in not found page

--- screens/NotFoundScreen.tsx
+++ screens/NotFoundScreen.tsx
@@ -1,40 +1,18 @@
 import * as React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 import { ParamListBase } from '@react-navigation/routers';
 import { StackScreenProps } from '@react-navigation/stack';
+import { Button } from 'react-native-paper';
+
+import ScreenContainer from '../components/ScreenContainer';
 
 
 export default function NotFoundScreen({
   navigation,
 }: StackScreenProps<ParamListBase, 'NotFound'>) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>This screen doesn't exist.</Text>
-      <TouchableOpacity onPress={() => navigation.replace('Root')} style={styles.link}>
-        <Text style={styles.linkText}>Go to home screen!</Text>
-      </TouchableOpacity>
-    </View>
+    <ScreenContainer title="Not found">
+      <Button onPress={() => navigation.replace('Root')}>Go to home screen!</Button>
+    </ScreenContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-  linkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
-});
