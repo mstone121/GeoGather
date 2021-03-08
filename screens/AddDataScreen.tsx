d1e501f2fd61f50d207a4ae9fcfc4887aef84ebe
Matt Stone <mstone@mrn.org>
2021-03-08 02:10:39 -0600

    Add data from add data screen

--- screens/AddDataScreen.tsx
+++ screens/AddDataScreen.tsx
@@ -1,16 +1,24 @@
 import * as React from "react";
+import { useDispatch } from "react-redux";
 import { StackScreenProps } from "@react-navigation/stack";
 
 import { BottomTabStackParams } from "../navigation/BottomTabNavigator";
 import ScreenContainer from "../components/ScreenContainer";
-import DataForm from "../components/DataForm";
+import DataForm, { FormValues } from "../components/DataForm";
+import { addData } from "../reducers/dataSlice";
 
 export default function AddDataScreen({
   navigation,
 }: StackScreenProps<BottomTabStackParams, "AddData">) {
+  const dispatch = useDispatch();
+
+  const onSubmit = (formValues: FormValues) => {
+    dispatch(addData(formValues));
+  };
+
   return (
     <ScreenContainer title="Add Data" navigation={navigation}>
-      <DataForm />
+      <DataForm onSubmit={onSubmit} />
     </ScreenContainer>
   );
 }
