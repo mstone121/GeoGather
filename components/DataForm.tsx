21a43effc772a8d80b0226f514ad48d987ddca9c
Matt Stone <mstone@mrn.org>
2021-03-08 00:20:38 -0600

    Update form options

--- components/DataForm.tsx
+++ components/DataForm.tsx
@@ -26,23 +26,39 @@ export default function DataForm({ formValues }: { formValues?: formValues }) {
         keyboardType="numeric"
       />
 
+      <Caption>Canopy Condition</Caption>
       <Picker
         selectedValue={canopyCondition}
         onValueChange={setCanopyCondition}
       >
-        <Picker.Item label="" value={undefined} />
+        <Picker.Item label="(n/a)" value={undefined} />
         <Picker.Item label="Full Canopy" value={0} />
         <Picker.Item label="Some Dead Branches" value={1} />
         <Picker.Item label="Mostly Dead" value={2} />
         <Picker.Item label="Completely Dead" value={3} />
       </Picker>
 
+      <Spacer />
+
+      <Caption>Bark Condition</Caption>
       <Picker selectedValue={barkCondition} onValueChange={setBarkCondition}>
-        <Picker.Item label="" value={undefined} />
+        <Picker.Item label="(n/a)" value={undefined} />
         <Picker.Item label="Intact" value={0} />
         <Picker.Item label="Light Damage" value={1} />
         <Picker.Item label="Heavy Damage" value={2} />
       </Picker>
+
+      <Caption>Location Type</Caption>
+      <Picker selectedValue={locationType} onValueChange={setLocationType}>
+        <Picker.Item label="(n/a)" value={undefined} />
+        <Picker.Item label="Forested Area" value="forest" />
+        <Picker.Item label="City Park" value="park" />
+        <Picker.Item label="Street" value="street" />
+      </Picker>
+
+      <Spacer />
+
+      <TextInput label="Notes" value={notes} onChangeText={setNotes} />
     </View>
   );
 }
