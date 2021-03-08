8ee97673eea43d8f44a6c06704c33d2993111612
Matt Stone <mstone@mrn.org>
2021-03-07 22:41:40 -0600

    Add pickers to form

--- components/DataForm.tsx
+++ components/DataForm.tsx
@@ -1,26 +1,49 @@
-import React, { useState, ReactChildren } from "react";
-import { Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import { TextInput } from 'react-native-paper';
 
 type formValues = {
-  bores: number
+  bores: number | undefined,
+  canopyCondition: number | undefined,
+  barkCondition: number | undefined
 }
 
 const convertToInt = (value: string) => parseInt(value.replace(/^\D+/g, '')) || undefined
 
 export default function DataForm({ formValues }: { formValues?: formValues }) {
   const [bores, setBores] = useState<number | undefined>();
+  const [canopyCondition, setCanopyCondition] = useState<number | undefined>();
+  const [barkCondition, setBarkCondition] = useState<number | undefined>();
 
   return (
     <View>
-      <InputLabel>Bores</InputLabel>
       <TextInput
+        label="Bore Count"
         value={bores?.toString() ?? ''}
         onChangeText={newValue => setBores(convertToInt(newValue))}
         keyboardType="numeric" />
-    </View>
-  );
-}
 
-function InputLabel({ children }: { children: string }) {
-  return <Text>{children}</Text>
+      <Picker
+        selectedValue={canopyCondition}
+        onValueChange={setCanopyCondition}
+      >
+        <Picker.Item label="" value={undefined} />
+        <Picker.Item label="Full Canopy" value={0} />
+        <Picker.Item label="Some Dead Branches" value={1} />
+        <Picker.Item label="Mostly Dead" value={2} />
+        <Picker.Item label="Completely Dead" value={3} />
+      </Picker>
+
+      <Picker
+        selectedValue={barkCondition}
+        onValueChange={setBarkCondition}
+      >
+        <Picker.Item label="" value={undefined} />
+        <Picker.Item label="Intact" value={0} />
+        <Picker.Item label="Light Damage" value={1} />
+        <Picker.Item label="Heavy Damage" value={2} />
+      </Picker>
+    </View >
+  );
 }
