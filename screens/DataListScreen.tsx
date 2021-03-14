802850fb843978db56302570124923746e739dfd
Matt Stone <mstone@mrn.org>
2021-03-14 15:54:17 -0500

    Optimize data list

--- screens/DataListScreen.tsx
+++ screens/DataListScreen.tsx
@@ -1,7 +1,7 @@
-import * as React from "react";
+import React, { useCallback } from "react";
 import { useSelector } from "react-redux";
 import { StackScreenProps } from "@react-navigation/stack";
-import { List } from "react-native-paper";
+import { Button, Card, List } from "react-native-paper";
 
 import { DataListStackParams } from "../navigation/BottomTabNavigator";
 import { RootState } from "../reducers";
@@ -13,7 +13,10 @@ export default function DataListScreen({
 }: StackScreenProps<DataListStackParams, "DataList">) {
   const dataList = useSelector((state: RootState) => state.data);
 
-  const editItem = (id: string) => navigation.navigate("EditData", { id });
+  const editItem = useCallback(
+    (id: string) => navigation.navigate("EditData", { id }),
+    [navigation]
+  );
 
   return (
     <ScreenContainer title="Data List" navigation={navigation}>
@@ -26,41 +29,45 @@ export default function DataListScreen({
   );
 }
 
-function DataListItem({
-  data,
-  editItem,
-}: {
-  data: Data;
-  editItem: (id: string) => void;
-}) {
-  return (
-    <List.Accordion
-      title={new Date(data.createdAt).toString()}
-      style={{ paddingLeft: 8 }}
-    >
-      <List.Item
-        title="Location"
-        description={
-          data.formValues.location
-            ? `${data.formValues.location?.lat}, ${data.formValues.location?.long}`
-            : ""
-        }
-      />
-      <List.Item title="Bores" description={data.formValues.bores ?? "(n/a)"} />
-      <List.Item
-        title="Canopy Condition"
-        description={data.formValues.canopyCondition ?? "(n/a)"}
-      />
-      <List.Item
-        title="Bark Condition"
-        description={data.formValues.barkCondition ?? "(n/a)"}
-      />
-      <List.Item
-        title="Location Type"
-        description={data.formValues.locationType ?? "(n/a)"}
-      />
-      <List.Item title="Notes" description={data.formValues.notes ?? "(n/a)"} />
-      <List.Item title="Edit" onPress={() => editItem(data.id)} />
-    </List.Accordion>
-  );
-}
+const DataListItem = React.memo(
+  ({ data, editItem }: { data: Data; editItem: (id: string) => void }) => {
+    return (
+      <List.Accordion title={new Date(data.createdAt).toString()}>
+        <Card>
+          <List.Item
+            title="Location"
+            description={
+              data.formValues.location
+                ? `${data.formValues.location?.lat}, ${data.formValues.location?.long}`
+                : ""
+            }
+          />
+          <List.Item
+            title="Bores"
+            description={data.formValues.bores ?? "(n/a)"}
+          />
+          <List.Item
+            title="Canopy Condition"
+            description={data.formValues.canopyCondition ?? "(n/a)"}
+          />
+          <List.Item
+            title="Bark Condition"
+            description={data.formValues.barkCondition ?? "(n/a)"}
+          />
+          <List.Item
+            title="Location Type"
+            description={data.formValues.locationType ?? "(n/a)"}
+          />
+          <List.Item
+            title="Notes"
+            description={data.formValues.notes ?? "(n/a)"}
+          />
+          <List.Item
+            title={<Button mode="contained">Edit</Button>}
+            onPress={() => editItem(data.id)}
+          />
+        </Card>
+      </List.Accordion>
+    );
+  }
+);
